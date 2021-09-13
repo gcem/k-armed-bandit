7328ecb280fc6711c07c4d68a7354ddd82128d58
gcem <34423323+gcem@users.noreply.github.com>
2021-09-13 16:11:35 +0300

    apply changes in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
-#include "ConstantRewardTestBed.hpp"
 #include "ChangingRewardTestBed.hpp"
+#include "ConstantRewardTestBed.hpp"
 #include "SampleAverageEpsilonGreedySolver.hpp"
 #include <string>
 
@@ -9,17 +9,10 @@ main()
     // this should normally not be set in main ^_^
     int arms = 10;
 
-<<<<<<< Updated upstream
-    ConstantRewardTestBed testBed(arms, 3000, 0, 1, 1);
-    SampleAverageEpsilonGreedySolver solver(arms, 0.1, 0);
-
-    auto results = testBed.test(&solver, 1000);
-    results.exportTxt("results_saeg.txt");
-=======
     // ConstantRewardTestBed testBed(arms, 10000, 0, 1, 1);
     ChangingRewardTestBed testBed(arms, 10000, 0, 0.0001, 1);
 
-    std::vector<double> epsilons {0, 0.01, 0.1};
+    std::vector<double> epsilons{ 0, 0.01, 0.1 };
 
     for (double epsilon : epsilons) {
         SampleAverageEpsilonGreedySolver solver(arms, epsilon, 0);
@@ -27,7 +20,6 @@ main()
         // TODO
         results1.exportTxt("ch_results_eps_0.10.txt");
     }
->>>>>>> Stashed changes
 
     return 0;
 }
