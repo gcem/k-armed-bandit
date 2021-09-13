0d14f5ce358962c1dd5dfd52f7dd5c59e51233fa
gcem <34423323+gcem@users.noreply.github.com>
2021-09-13 16:07:27 +0300

    Add test bed for nonstationary problems (main is not completed)

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include "ConstantRewardTestBed.hpp"
+#include "ChangingRewardTestBed.hpp"
 #include "SampleAverageEpsilonGreedySolver.hpp"
+#include <string>
 
 int
 main()
@@ -7,11 +9,25 @@ main()
     // this should normally not be set in main ^_^
     int arms = 10;
 
+<<<<<<< Updated upstream
     ConstantRewardTestBed testBed(arms, 3000, 0, 1, 1);
     SampleAverageEpsilonGreedySolver solver(arms, 0.1, 0);
 
     auto results = testBed.test(&solver, 1000);
     results.exportTxt("results_saeg.txt");
+=======
+    // ConstantRewardTestBed testBed(arms, 10000, 0, 1, 1);
+    ChangingRewardTestBed testBed(arms, 10000, 0, 0.0001, 1);
+
+    std::vector<double> epsilons {0, 0.01, 0.1};
+
+    for (double epsilon : epsilons) {
+        SampleAverageEpsilonGreedySolver solver(arms, epsilon, 0);
+        auto results3 = testBed.test(&solver3, 1000);
+        // TODO
+        results1.exportTxt("ch_results_eps_0.10.txt");
+    }
+>>>>>>> Stashed changes
 
     return 0;
 }
